a8b66ad8c18967ab97b33af5410210cd6510632b
free@action <fengrui358@163.com>
2016-09-02 13:23:47 +0800

    准备测试下三个不同的.Net平台下的压缩库

---
+++ ZipLabs/ZipLabs/DotNetZip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipLabs
+{
+    /// <summary>
+    /// https://github.com/haf/DotNetZip.Semverd
+    /// </summary>
+    class DotNetZip
+    {
+    }
+}
