6642d2dcf921b7e8d6551620995ab22c1cac9b95
free@action98.com <free@action98.com>
2016-09-26 22:41:08 +0800

    SocketDemo

---
+++ SocketLabs/SimpleSocketLab/SimpleSocketClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("启动客户端");
+
+            var ip = ConfigurationManager.AppSettings["RemoteIp"];
+            var port = int.Parse(ConfigurationManager.AppSettings["RemotePort"]);
+
+            var localPort = int.Parse(ConfigurationManager.AppSettings["Port"]);
+
+            Console.WriteLine($"服务器地址：{ip}:{port}");
+
+            var ipAddress = IPAddress.Parse(ip);
+            var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            client.Bind(new IPEndPoint(IPAddress.Any, localPort));
+
+            EndPoint remoteEndPoint = new IPEndPoint(ipAddress, port);
+            try
+            {
+                client.Connect(remoteEndPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("连接服务失败！");
+            }
+
+            var receiveResult = new byte[1024];
+            var receiveNumber = client.ReceiveFrom(receiveResult, ref remoteEndPoint);            
+
+            Console.WriteLine($"接收到服务端消息：{Encoding.UTF8.GetString(receiveResult, 0, receiveNumber)}");
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                var sendMsg = $"{Guid.NewGuid()}，时间：{DateTime.Now}";
+                client.Send(Encoding.UTF8.GetBytes(sendMsg));
+
+                Console.WriteLine($"向服务端发送消息：{sendMsg}");
+            }
+        }
+    }
+}
---
+++ SocketLabs/SimpleSocketLab/SimpleSocketServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SimpleSocketServer
+{
+    class Program
+    {
+        private static Socket _serverSocket;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("启动服务");
+
+            var port = int.Parse(ConfigurationManager.AppSettings["Port"]);
+
+            Console.WriteLine($"地址：{IPAddress.Any}:{port}");
+
+            _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            _serverSocket.Listen(10);
+
+            var t = new Thread(ListenClientConnect);
+            t.Start();
+
+            Console.ReadKey();
+        }
+
+        private static void ListenClientConnect()
+        {
+            while(true)
+            {
+                var socketClient = _serverSocket.Accept();
+                socketClient.Send(Encoding.UTF8.GetBytes("Hello World"));
+
+                var t = new Thread(ReceiveMessage);
+                t.Start(socketClient);
+            }
+        }
+
+        private static void ReceiveMessage(object socketClient)
+        {
+            var client = (Socket) socketClient;
+            var receiveReult = new byte[1024];
+            while (true)
+            {
+                try
+                {
+                    int receiveNumber = client.Receive(receiveReult);
+                    Console.WriteLine(
+                        $"接受到客户端{client.RemoteEndPoint}的消息：{Encoding.UTF8.GetString(receiveReult, 0, receiveNumber)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
