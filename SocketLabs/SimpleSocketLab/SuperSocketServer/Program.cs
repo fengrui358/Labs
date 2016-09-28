fd80f00f08ef3174cd1c417c60c34337773c2c6d
free@action98.com <free@action98.com>
2016-09-28 09:00:56 +0800

    增加SuperSocket的工程

---
+++ SocketLabs/SimpleSocketLab/SuperSocketServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("服务启动");
+            var server = new TelnetServer();
+            server.Setup(4567);
+            
+            server.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketLabs/SimpleSocketLab/SuperSocketServer/TelnetServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.SocketBase;
+
+namespace SuperSocketServer
+{
+    class TelnetServer : AppServer<TelnetSession>
+    {
+    }
+}
