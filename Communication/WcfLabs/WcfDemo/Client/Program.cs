df54d9d437b59fba67251fc9b7950cae327197a8
free@action <fengrui358@163.com>
2016-10-20 16:04:31 +0800

    增加一个WCF的Demo

---
+++ Communication/WcfLabs/WcfDemo/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Interface;
+
+namespace Client
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            ICalculator proxy = GetProxy<ICalculator>();
+            Console.WriteLine(proxy.Add(3, 6));
+            Console.WriteLine(proxy.Subtract(3, 6));
+            Console.WriteLine(proxy.Multiply(3, 6));
+            Console.WriteLine(proxy.Divide(3, 6));
+
+            IHelloWord proxy2 = GetProxy<IHelloWord>();
+            Console.WriteLine(proxy2.HelloWord());
+            Console.WriteLine(proxy2.HelloWord("sadfasfsafasdfassafsadf"));
+
+            Console.Read();
+        }
+
+        private static T GetProxy<T>()
+        {
+            ChannelFactory<T> channelFactory = new ChannelFactory<T>(new WSHttpBinding(),
+                $"http://192.168.1.80:3721/{typeof (T).FullName}");
+
+            
+            return channelFactory.CreateChannel();
+        }
+    }
+}
---
+++ Communication/WcfLabs/WcfDemo/Service/HelloWordService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service.Interface;
+
+namespace Service
+{
+    public class HelloWordService : IHelloWord
+    {
+        public string HelloWord()
+        {
+            return "Hello Word!!!";
+        }
+
+        public string HelloWord(string s)
+        {
+            return s;
+        }
+    }
+}
