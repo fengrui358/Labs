cf7d6604823b69026b9e8d8484a1e3b4dbbd4ed2
秦晋 <qjdev@qq.com>
2016-11-24 21:21:44 +0800

    增加结构体与数组转换Demo

---
+++ CSharpLanguageLabs/结构体字节数组转换/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 结构体字节数组转换
+{
+    //注意这个属性不能少
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    struct TestStruct
+    {
+        public int c;
+
+        //字符串，SizeConst为字符串的最大长度
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)] public string str;
+
+        //int数组，SizeConst表示数组的个数，在转换成
+        //byte数组前必须先初始化数组，再使用，初始化
+        //的数组长度必须和SizeConst一致，例test = new int[6];
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] test;
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testStruct = new TestStruct {c = 55, str = "Hello World!", test = new[] {23, 56, 76, 34, 56, 87}};
+
+            var bytes = StructToBytes(testStruct);
+
+            var newStruct = (TestStruct)BytesToStuct(bytes, typeof(TestStruct));
+        }
+
+        /// <summary>
+        /// 结构体转byte数组
+        /// </summary>
+        /// <param name="structObj">要转换的结构体</param>
+        /// <returns>转换后的byte数组</returns>
+        public static byte[] StructToBytes(object structObj)
+        {
+            //得到结构体的大小
+            int size = Marshal.SizeOf(structObj);
+            //创建byte数组
+            byte[] bytes = new byte[size];
+            //分配结构体大小的内存空间
+            IntPtr structPtr = Marshal.AllocHGlobal(size);
+            //将结构体拷到分配好的内存空间
+            Marshal.StructureToPtr(structObj, structPtr, false);
+            //从内存空间拷到byte数组
+            Marshal.Copy(structPtr, bytes, 0, size);
+            //释放内存空间
+            Marshal.FreeHGlobal(structPtr);
+            //返回byte数组
+            return bytes;
+        }
+
+        /// <summary>
+        /// byte数组转结构体
+        /// </summary>
+        /// <param name="bytes">byte数组</param>
+        /// <param name="type">结构体类型</param>
+        /// <returns>转换后的结构体</returns>
+        public static object BytesToStuct(byte[] bytes, Type type)
+        {
+            //得到结构体的大小
+            int size = Marshal.SizeOf(type);
+            //byte数组长度小于结构体的大小
+            if (size > bytes.Length)
+            {
+                //返回空
+                return null;
+            }
+            //分配结构体大小的内存空间
+            IntPtr structPtr = Marshal.AllocHGlobal(size);
+            //将byte数组拷到分配好的内存空间
+            Marshal.Copy(bytes, 0, structPtr, size);
+            //将内存空间转换为目标结构体
+            object obj = Marshal.PtrToStructure(structPtr, type);
+            //释放内存空间
+            Marshal.FreeHGlobal(structPtr);
+            //返回结构体
+            return obj;
+        }
+    }
+}
