759981b9a4bbd280226ec6f4455ca9e4db55b3ff
秦晋 <qjdev@qq.com>
2016-12-29 19:29:05 +0800

    增加FTP服务和客户端测试，未完成

--- Communication/FtpLabs/FtpClient/Program.cs
+++ Communication/FtpLabs/FtpClient/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.FtpClient;
+using WindowsFormsApplication1;
 
 namespace FtpClient
 {
@@ -10,6 +12,17 @@ namespace FtpClient
     {
         static void Main(string[] args)
         {
+            //var ftpClient = new System.Net.FtpClient.FtpClient();
+            //ftpClient.Host = "192.168.1.80";
+            //ftpClient.Port = 21;
+
+            //ftpClient.
+
+            var ftpClient = new FTPClient();
+            ftpClient.RemoteHost = "192.168.1.80";
+            ftpClient.RemotePort = 21;
+
+            ftpClient.Connect();
         }
     }
 }
--- Communication/FtpLabs/FtpServer(SuperSocket)/Program.cs
+++ Communication/FtpLabs/FtpServer(SuperSocket)/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SuperSocket.Ftp.FtpService;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketEngine;
 
 namespace FtpLabs
 {
@@ -10,6 +14,41 @@ namespace FtpLabs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Start the server!");
+
+            var bootstrap = BootstrapFactory.CreateBootstrapFromConfigFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Config\Basic.config"));
+
+            if (!bootstrap.Initialize())
+            {
+                Console.WriteLine("Failed to initialize!");
+                Console.ReadKey();
+                return;
+            }
+
+            var result = bootstrap.Start();
+
+            Console.WriteLine("Start result: {0}!", result);
+
+            if (result == StartResult.Failed)
+            {
+                Console.WriteLine("Failed to start!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Press key 'q' to stop it!");
+
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine();
+                continue;
+            }
+
+            Console.WriteLine();
+
+            //Stop the appServer
+            bootstrap.Stop();
+            Console.ReadKey();
         }
     }
 }
