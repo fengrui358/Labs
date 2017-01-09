c03a630ea8939e4a632ca7b414441965c8781580
free@action98.com <free@action98.com>
2017-01-09 13:02:22 +0800

    暂时提交微信小程序相关内容

--- WeiXin/MyDemo/pages/component/index.js
+++ WeiXin/MyDemo/pages/component/index.js
@@ -1,36 +1,59 @@
 // pages/component/index.js
 Page({
-  data:{
+  data: {
+    list: [
+      {
+        id: "view",
+        name: "视图容器",
+        open: false,
+        pages: ["view", "scroll-view", "swiper"]
+      }
+    ]
   },
-  onLoad:function(options){
+  widgetsToggle: function (e) {
+    var id = e.currentTarget.id;
+    var dataList = this.data.list;
+    for (var i = 0; i < dataList.length; i++) {
+      if (dataList[i].id == id) {
+        dataList[i].open = !dataList[i].open;
+      }
+      else {
+        dataList[i].open = false;
+      }
+    }
+
+    this.setData({ list: dataList });
+  },
+
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     console.log("index onLoad");
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
     console.log("index onReady");
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
     console.log("index onShow");
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
     console.log("index onHide");
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
     console.log("index onUnload");
   },
-  onPullDownRefresh:function(){
+  onPullDownRefresh: function () {
     // 用户下拉动作
     console.log("index onPullDownRefresh");
   },
-  onReachBottom:function(){
+  onReachBottom: function () {
     // 页面上拉触底
     console.log("index onReachBottom");
   },
-  onShareAppMessage:function(){
+  onShareAppMessage: function () {
     // 用户点击右上角分享
     console.log("index onShareAppMessage");
   }
