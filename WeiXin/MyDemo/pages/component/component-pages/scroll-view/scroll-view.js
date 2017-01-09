2233cb99605b631aa61571114810e4a1eaad4b29
free@action <fengrui358@163.com>
2017-01-09 16:07:45 +0800

    上下滚动视图

---
+++ WeiXin/MyDemo/pages/component/component-pages/scroll-view/scroll-view.js
@@ -0,0 +1,37 @@
+// pages/component/component-pages/scroll-view/scroll-view.js
+var order = ['green', 'red', 'yellow', 'blue', 'green']
+
+Page({
+  data:{
+    toView: 'green'
+  },
+  upper: function(e) {
+    console.log(e)
+  },
+  lower: function(e) {
+    console.log(e)
+  },
+  scroll: function(e) {
+    console.log(e)
+  },
+  scrollToTop: function(e) {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
