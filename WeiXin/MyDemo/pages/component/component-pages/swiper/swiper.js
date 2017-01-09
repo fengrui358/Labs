10eb9e195134149b0a8655fe15ffce3a9c6a3ce0
free@action <fengrui358@163.com>
2017-01-09 16:53:27 +0800

    增加swiper组件

---
+++ WeiXin/MyDemo/pages/component/component-pages/swiper/swiper.js
@@ -0,0 +1,51 @@
+// pages/component/component-pages/swiper/swiper.js
+Page({
+  data: {
+    background: ['green', 'red', 'yellow'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 600
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeVertical: function (e) {
+    this.setData({
+      vertical: !this.data.vertical
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
