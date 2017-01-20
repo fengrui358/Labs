93e691a3271b76766f10a4bf288a9fab99c16ffd
free@action <fengrui358@163.com>
2017-01-20 11:48:27 +0800

    加入事件订阅内存引用的测试

---
+++ CSharpLanguageLabs/EventSubscribeLab/Lab.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSubscribeLab
+{
+    class Lab
+    {
+        private Subscriber _subscriber;
+        private Publisher _publisher;
+
+        private WeakReference<Subscriber> _subscriberWeakReference;
+        private WeakReference<Publisher> _publisherWeakReference;
+
+        public Lab()
+        {
+            _subscriber = new Subscriber();
+            _publisher = new Publisher();
+
+            _subscriber.Subscribe(_publisher);
+            _subscriberWeakReference = new WeakReference<Subscriber>(_subscriber);
+            _publisherWeakReference = new WeakReference<Publisher>(_publisher);
+        }
+
+        public void ReleaseSubscriber()
+        {
+            _subscriber = null;
+            GC.Collect();
+        }
+
+        public void ReleasePublisher()
+        {
+            _publisher = null;
+            GC.Collect();
+        }
+
+        public bool TestExistSubscriber()
+        {
+            Subscriber outSubscriber;
+            return _subscriberWeakReference != null && _subscriberWeakReference.TryGetTarget(out outSubscriber);
+        }
+
+        public bool TestExistPublisher()
+        {
+            Publisher outPublisher;
+            return _publisherWeakReference != null && _publisherWeakReference.TryGetTarget(out outPublisher);
+        }
+    }
+
+    /// <summary>
+    /// 事件订阅者
+    /// </summary>
+    class Subscriber
+    {
+        public void Subscribe(Publisher publisher)
+        {
+            publisher.TestEvent += PublisherOnTestEvent;   
+        }
+
+        private void PublisherOnTestEvent(object sender, EventArgs eventArgs)
+        {
+        }
+    }
+
+    class Publisher
+    {
+        public event EventHandler TestEvent;
+
+        protected virtual void OnTestEvent()
+        {
+            TestEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ CSharpLanguageLabs/EventSubscribeLab/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSubscribeLab
+{
+    /// <summary>
+    /// 测试事件订阅影响GC回收
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(1);
+            Test(2);
+            Test(3);
+
+            Console.WriteLine("结论：如果仅仅释放订阅者，发布者还存在，那么订阅者的内存并不会被GC回收！！需要重点注意！！");
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 进行实验，key为1时释放发布者，key为2时释放订阅者，key为3时释放两者
+        /// </summary>
+        /// <param name="key"></param>
+        private static void Test(int key)
+        {
+            var lab = new Lab();
+            PrintExist(lab);
+
+            switch (key)
+            {
+                //key为1时释放发布者
+                case 1:
+                    Console.WriteLine("释放发布者");
+                    lab.ReleasePublisher();
+                    break;
+                //key为2时释放订阅者
+                case 2:
+                    Console.WriteLine("释放订阅者");
+                    lab.ReleaseSubscriber();
+                    break;
+                //key为3时释放两者
+                case 3:
+                    Console.WriteLine("释放两者");
+                    lab.ReleasePublisher();
+                    lab.ReleaseSubscriber();
+                    break;
+
+            }
+            
+            PrintExist(lab);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 打印存在情况
+        /// </summary>
+        private static void PrintExist(Lab lab)
+        {
+            Console.WriteLine($"发布者是否存在：{lab.TestExistPublisher()}---订阅者是否存在：{lab.TestExistSubscriber()}");
+        }
+    }
+}
