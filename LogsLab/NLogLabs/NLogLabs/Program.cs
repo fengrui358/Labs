d96fb6520ae03ad430b0006c53ce1599476aa020
秦晋 <qjdev@qq.com>
2017-05-10 22:43:56 +0800

    增加邮件日志测试

---
+++ LogsLab/NLogLabs/NLogLabs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLogLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = LogManager.GetCurrentClassLogger();
+
+            log.Debug("邮件日志测试");
+
+            Console.Read();
+        }
+    }
+}
