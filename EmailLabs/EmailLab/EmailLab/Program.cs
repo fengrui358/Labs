7133cbf293bbf3271299e76da60fac35142bd9d9
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2017-10-11 18:12:37 +0800

    初始化Email测试

---
+++ EmailLabs/EmailLab/EmailLab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
