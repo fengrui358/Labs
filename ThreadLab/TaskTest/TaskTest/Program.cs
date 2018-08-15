775fb7cab5dfd6a7d8e1a40be89de85b4ea134fe
free <devqj@qq.com>
2018-08-15 23:01:47 +0800

    增加Task的AttachToParent的Demo

---
+++ ThreadLab/TaskTest/TaskTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace TaskTest
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                var result = AsyncContext.Run(() => MainAsync(args));
+                Console.Read();
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+        }
+
+        static async Task<int> MainAsync(string[] args)
+        {
+            await TaskAttachedToParentTest.ParentTask();
+            return 1;
+        }
+    }
+}
---
+++ ThreadLab/TaskTest/TaskTest/TaskAttachedToParentTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+    /// <summary>
+    /// 《C#并发编程经典实例》 P41
+    /// </summary>
+    public class TaskAttachedToParentTest
+    {
+        public static async Task ParentTask()
+        {
+            await Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine(
+                        $"{DateTime.Now:HH:mm:ss ffff} Parent task begin, id is {Thread.CurrentThread.ManagedThreadId}");
+
+                    StartSubTask();
+                });
+
+            Console.WriteLine(
+                $"{DateTime.Now:HH:mm:ss ffff} Parent task end, id is {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static void StartSubTask()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine(
+                    $"{DateTime.Now:HH:mm:ss ffff} Child task begin, id is {Thread.CurrentThread.ManagedThreadId}");
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine(
+                    $"{DateTime.Now:HH:mm:ss ffff} Child task end, id is {Thread.CurrentThread.ManagedThreadId}");
+            }, TaskCreationOptions.AttachedToParent);
+            //}, TaskCreationOptions.None);  如果不设置AttachedToParent父线程不会等待就直接结束
+        }
+    }
+}
