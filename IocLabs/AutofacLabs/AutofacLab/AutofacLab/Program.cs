b9410225855f8648c3486610e8be7060749fc4d9
free <devqj@qq.com>
2019-01-31 11:19:44 -0400

    init

---
+++ IocLabs/AutofacLabs/AutofacLab/AutofacLab/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AutofacLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lab1 = new Lab1();
+            lab1.WriteDate();
+            lab1.WriteDate();
+
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
