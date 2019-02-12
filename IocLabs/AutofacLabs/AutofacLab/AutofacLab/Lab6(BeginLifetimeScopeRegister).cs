dd0018d69d6d78f977f0e1d63ca0d73c66393d56
free <devqj@qq.com>
2019-02-12 16:06:14 -0400

    添加示例

---
+++ IocLabs/AutofacLabs/AutofacLab/AutofacLab/Lab6(BeginLifetimeScopeRegister).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Autofac;
+
+namespace AutofacLab
+{
+    public class Lab6
+    {
+        private readonly IContainer _container;
+
+        public Lab6()
+        {
+            var builder = new ContainerBuilder();
+
+            _container = builder.Build();
+        }
+
+        public void WriteString()
+        {
+            using (var scope = _container.BeginLifetimeScope(
+                b => b.RegisterType<Lab6Class>().AsSelf()))
+            {
+                Console.WriteLine(scope.Resolve<Lab6Class>().Get());
+            }
+
+            Console.WriteLine(value: _container.ResolveOptional<Lab6Class>()?.Get());
+        }
+    }
+
+    public class Lab6Class
+    {
+        public string Get()
+        {
+            return nameof(Lab6Class);
+        }
+    }
+}
--- IocLabs/AutofacLabs/AutofacLab/AutofacLab/Program.cs
+++ IocLabs/AutofacLabs/AutofacLab/AutofacLab/Program.cs
@@ -9,18 +9,15 @@ namespace AutofacLab
             var lab1 = new Lab1();
             lab1.WriteDate();
             lab1.WriteDate();
-
             Console.WriteLine();
 
             var lab2 = new Lab2();
             lab2.WriteString();
-
             Console.WriteLine();
 
             var lab3 = new Lab3();
             lab3.WriteDate();
             lab3.WriteSelfDate();
-
             Console.WriteLine();
 
             //var lab4 = new Lab4();
@@ -28,7 +25,10 @@ namespace AutofacLab
 
             var lab5 = new Lab5();
             lab5.WriteString();
+            Console.WriteLine();
 
+            var lab6 = new Lab6();
+            lab6.WriteString();
             Console.WriteLine();
 
             //Console.WriteLine("Hello World!");
