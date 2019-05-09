c8e9fefb8e5c95f49b17d52609d3ead82c632d46
free <devqj@qq.com>
2019-05-09 23:30:18 +0800

    add CSharpLanguageLabs/NumericalSizeLab/NumericalSizeLab.csproj

---
+++ CSharpLanguageLabs/NumericalSizeLab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NumericalSizeLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{nameof(Single)}.{nameof(Marshal.SizeOf)}:{Marshal.SizeOf<Single>()}");
+            Console.WriteLine($"{nameof(Double)}.{nameof(Marshal.SizeOf)}:{Marshal.SizeOf<Double>()}");
+            Console.WriteLine($"{nameof(Int32)}.{nameof(Marshal.SizeOf)}:{Marshal.SizeOf<Int32>()}");
+            Console.WriteLine($"{nameof(Int64)}.{nameof(Marshal.SizeOf)}:{Marshal.SizeOf<Int64>()}");
+            Console.WriteLine();
+
+            Console.WriteLine($"{nameof(Single)}.{nameof(Single.MinValue)}:{Single.MinValue}");
+            Console.WriteLine($"{nameof(Double)}.{nameof(Double.MinValue)}:{Double.MinValue}");
+            Console.WriteLine($"{nameof(Int32)}.{nameof(Int32.MinValue)}:{Int32.MinValue}");
+            Console.WriteLine($"{nameof(Int64)}.{nameof(Int64.MinValue)}:{Int64.MinValue}");
+            Console.WriteLine($"{nameof(Single)}.{nameof(Single.MaxValue)}:{Single.MaxValue}");
+            Console.WriteLine($"{nameof(Double)}.{nameof(Double.MaxValue)}:{Double.MaxValue}");
+            Console.WriteLine($"{nameof(Int32)}.{nameof(Int32.MaxValue)}:{Int32.MaxValue}");
+            Console.WriteLine($"{nameof(Int64)}.{nameof(Int64.MaxValue)}:{Int64.MaxValue}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DapperDemo/Dapper.NetCore.Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Dapper.FastCrud;
+
+namespace Dapper.NetCore.Demo
+{
+    /// <summary>
+    /// https://github.com/MoonStorm/Dapper.FastCRUD
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //设置默认参数
+            OrmConfiguration.DefaultDialect = SqlDialect.SqLite;
+            OrmConfiguration.DefaultSqlStatementOptions.CommandTimeout = TimeSpan.FromSeconds(30);
+            
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
