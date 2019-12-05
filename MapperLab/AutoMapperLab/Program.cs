8cd6d5e8e7f81c7ef4dff380987ad34fc6922f1a
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2019-12-05 17:57:40 +0800

    增加测试代码

---
+++ MapperLab/AutoMapperLab/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using AutoMapper;
+using Xunit;
+
+namespace AutoMapperLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<A, A>();
+            }));
+
+            #region 直接Copy
+
+            var a = new A
+            {
+                AString = "asfFDSF345FDSfsdf"
+            };
+
+            var a2 = mapper.Map<A>(a);
+            Assert.NotEqual(a, a2);
+            Assert.Equal(a.AString, a2.AString);
+
+            #endregion
+
+            #region 驼峰转换
+
+
+
+            #endregion
+
+            #region 属性名转换
+
+
+
+            #endregion
+
+            #region 枚举和Int转换
+
+            
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+
+    class A
+    {
+        public string AString { get; set; }
+    }
+
+    class B
+    {
+        public string aString { get; set; }
+    }
+
+
+}
