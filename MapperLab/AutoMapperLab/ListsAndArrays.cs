e33d5ce0cb9425ac714df92928ea115951a93e82
free <devqj@qq.com>
2019-12-22 20:53:49 +0800

    集合自动映射子类型

--- MapperLab/AutoMapperLab/ListsAndArrays.cs
+++ MapperLab/AutoMapperLab/ListsAndArrays.cs
@@ -13,12 +13,18 @@ namespace AutoMapperLab
     {
         public void Test()
         {
-            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<Source, Destination>());
+            var configuration = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<Source, Destination>();
+                    cfg.CreateMap<ParentSource, ParentDestination>().Include<ChildSource, ChildDestination>();
+                    cfg.CreateMap<ChildSource, ChildDestination>();
+                }
+            );
             var sources = new[]
             {
-                new Source { Value = 5 },
-                new Source { Value = 6 },
-                new Source { Value = 7 }
+                new Source {Value = 5},
+                new Source {Value = 6},
+                new Source {Value = 7}
             };
 
             var mapper = new Mapper(configuration);
@@ -33,6 +39,19 @@ namespace AutoMapperLab
             {
                 Assert.NotEqual(ilistDest[i].GetHashCode(), sources[i].GetHashCode());
             }
+
+            var sources2 = new[]
+            {
+                new ParentSource(),
+                new ChildSource(),
+                new ParentSource()
+            };
+
+            var destinations = mapper.Map<ParentSource[], ParentDestination[]>(sources2);
+
+            Assert.IsType<ParentDestination>(destinations[0]);
+            Assert.IsType<ChildDestination>(destinations[1]);
+            Assert.IsType<ParentDestination>(destinations[2]);
         }
 
         class Source
@@ -43,6 +62,26 @@ namespace AutoMapperLab
         class Destination
         {
             public int Value { get; set; }
+        }
+
+        class ParentSource
+        {
+            public int Value1 { get; set; }
+        }
+
+        class ChildSource : ParentSource
+        {
+            public int Value2 { get; set; }
+        }
+
+        class ParentDestination
+        {
+            public int Value1 { get; set; }
+        }
+
+        class ChildDestination : ParentDestination
+        {
+            public int Value2 { get; set; }
         }
     }
 }
