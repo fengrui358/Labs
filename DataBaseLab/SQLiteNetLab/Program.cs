cee5ac84fa4d044df92f783bfb7bda8ff2e1dab5
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2019-12-24 15:57:12 +0800

    测试一个轻量级的SQLite库

---
+++ DataBaseLab/SQLiteNetLab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace SQLiteNetLab
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cadlocaldata.db");
+
+            var cn = new SQLiteAsyncConnection(path);
+
+            var all = await cn.Table<ReadUnread>().ToListAsync();
+
+            var allByUser =
+                await cn.Table<ReadUnread>().Where(s => s.UserId == "1205041586367389698").ToListAsync();
+
+            var result = await cn.Table<ReadUnread>().Where(s => s.UserId == "1205041586367389698" && s.Key == "1204674403909099522").ToListAsync();
+
+            await cn.CloseAsync();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataBaseLab/SQLiteNetLab/ReadUnread.cs
@@ -0,0 +1,17 @@
+﻿namespace SQLiteNetLab
+{
+    public class ReadUnread
+    {
+        public string ID { get; set; }
+        public string UserId { get; set; }
+        public TestEnum DataType { get; set; }
+        public string Key { get; set; }
+        public System.DateTime ReadTime { get; set; }
+    }
+
+    public enum TestEnum
+    {
+        A = 0,
+        B = 1
+    }
+}
