23b17ea30acec64daa492a57e75948320b782cc0
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-06-30 17:16:13 +0800

    rabbitMQ最基础demo

---
+++ Communication/RabbitMQDemo/RabbitMQReceiveDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQReceiveDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "10.15.9.113",
+                Port = 5672,
+                VirtualHost = "CAD_CLIENT",
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: true,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello",
+                autoAck: true,
+                consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Communication/RabbitMQDemo/RabbitMQSendDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQSendDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "10.15.9.113", Port = 5672, VirtualHost = "CAD_CLIENT", UserName = "admin", Password = "admin",
+                AutomaticRecoveryEnabled = true
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            //声明一个队列
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: true, arguments: null);
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    await Task.Delay(3000);
+                }
+            });
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
