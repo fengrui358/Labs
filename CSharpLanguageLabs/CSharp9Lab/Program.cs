725a5f4ad036e804bfcc6208fcfc6f3392beaafd
fengrui <fengrui358@163.com>
2020-11-15 23:36:42 +0800

    建立 .net 5.0 工程，但编译问题

---
+++ CSharpLanguageLabs/CSharp9Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp9Lab
+{
+    /// <summary>
+    /// https://devblogs.microsoft.com/dotnet/c-9-0-on-the-record/
+    /// https://www.cnblogs.com/tcjiaan/p/13947928.html
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Record
+
+            Console.WriteLine("Hello World!");
+
+            #endregion
+        }
+    }
+}
