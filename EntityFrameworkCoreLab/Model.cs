1b236b4766cdde5bc527ea2e0e726342717bb408
fengrui <fengrui358@163.com>
2021-03-29 14:51:04 +0800

    初始化数据库 1. blog 表 2. post 表

--- EntityFrameworkCoreLab/Model.cs
+++ EntityFrameworkCoreLab/Model.cs
@@ -3,14 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkCoreLab
 {
+    /// <summary>
+    /// dotnet tool install --global dotnet-ef
+    /// dotnet add package Microsoft.EntityFrameworkCore.Design
+    /// dotnet ef migrations add InitialCreate
+    /// dotnet ef database update
+    /// </summary>
     public class BloggingContext : DbContext
     {
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
         // The following configures EF to create a Sqlite database file as `C:\blogging.db`.
         // For Mac or Linux, change this to `/tmp/blogging.db` or any other absolute path.
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite(@"Data Source=C:\blogging.db");
+            => options.UseSqlite(@"Data Source=D:\blogging.db");
     }
     public class Blog
     {
