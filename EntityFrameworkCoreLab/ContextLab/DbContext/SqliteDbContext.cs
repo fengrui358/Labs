2d45a7e8b90473eca98792bf6fe873b6ed75a032
fengrui <fengrui358@163.com>
2021-04-20 13:55:28 +0800

    增加 sqlite db context

---
+++ EntityFrameworkCoreLab/ContextLab/DbContext/SqliteDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ContextLab.DbContext
+{
+    public class SqliteDbContext : DefaultDbContext
+    {
+        public SqliteDbContext(DbContextOptions<DefaultDbContext> options, IConfiguration configuration) : base(options, configuration)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=my.db");
+        }
+    }
+}
