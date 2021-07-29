c482d9a3dd563324a368202a6ef5cc94b691c5eb
fengrui <fengrui358@163.com>
2021-07-29 23:59:54 +0800

    Test

---
+++ LinqLab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LinqLab.Where;
+
+namespace LinqLab
+{
+    /// <summary>
+    /// https://www.tutorialsteacher.com/linq/linq-tutorials
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{nameof(Where1)}:{Environment.NewLine}");
+            new Where1().Test();
+            Console.WriteLine();
+
+            Console.WriteLine($"{nameof(Where2)}:{Environment.NewLine}");
+            new Where2().Test();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinqLab/Where/Where2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqLab.BaseModel;
+
+namespace LinqLab.Where
+{
+    public class Where2
+    {
+        public void Test()
+        {
+            // Student collection
+            IList<Student> studentList = new List<Student>()
+            {
+                new Student() {StudentID = 1, StudentName = "John", Age = 13},
+                new Student() {StudentID = 2, StudentName = "Moin", Age = 21},
+                new Student() {StudentID = 3, StudentName = "Bill", Age = 18},
+                new Student() {StudentID = 4, StudentName = "Ram", Age = 20},
+                new Student() {StudentID = 5, StudentName = "Ron", Age = 15}
+            };
+
+            Func<Student, bool> filterAge = s => s.Age > 12 && s.Age < 20;
+
+            // LINQ Query Syntax to find out teenager students
+            var teenAgerStudent = from s in studentList
+                where filterAge(s)
+                select s;
+            Console.WriteLine("Teen age Students:");
+
+            foreach (Student std in teenAgerStudent)
+            {
+                Console.WriteLine(std.StudentName);
+            }
+        }
+    }
+}
