36798df51f8e016773bfd609665eaae76d8f93f0
fengrui <fengrui358@163.com>
2021-09-08 10:08:47 +0800

    顺序 UUID

---
+++ IdentityServer/IdentityServer4/1_client_credentials/quickstart/src/IdentityServerAspNetIdentity/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+
+namespace IdentityServerAspNetIdentity
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant);
+            return Task.CompletedTask;
+        }
+    }
+}
