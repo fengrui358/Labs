3bec462d7a83a212813302222453c05a72bd11af
fengrui <fengrui358@163.com>
2021-10-26 20:50:15 +0800

    优化测试

--- Communication/RabbitMQDemo/RabbitMQReceiveDemo/Program.cs
+++ Communication/RabbitMQDemo/RabbitMQReceiveDemo/Program.cs
@@ -19,69 +19,44 @@ namespace RabbitMQReceiveDemo
                 AutomaticRecoveryEnabled = true
             };
 
+            var filterKey = "";
+
             using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            channel.QueueDeclare(queue: "free",
+            Listen(connection, "ErExchangeTopic", filterKey);
+            Listen(connection, "ErExchangeFanout", filterKey);
+            Listen(connection, "SyncDataExchangeTopic", filterKey);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static IModel Listen(IConnection connection, string exchangeName, string filterKey = "", string routingKey = "#")
+        {
+            var queueName = $"{exchangeName}_queue";
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: queueName,
                 durable: false,
                 exclusive: false,
                 autoDelete: true,
                 arguments: null);
 
-            channel.QueueBind("free", "ErExchangeTopic", "#");
+            channel.QueueBind(queueName, exchangeName, routingKey);
 
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(" [x] Received ErExchangeTopic {0}", message);
+                if (message.Contains(filterKey))
+                {
+                    Console.WriteLine($" [x] Received {exchangeName} {message}");
+                }
             };
-            channel.BasicConsume(queue: "free",
+            channel.BasicConsume(queue: queueName,
                 autoAck: true,
                 consumer: consumer);
 
-            using var channel2 = connection.CreateModel();
-            channel2.QueueDeclare(queue: "free2",
-                durable: false,
-                exclusive: false,
-                autoDelete: true,
-                arguments: null);
-
-            channel2.QueueBind("free2", "ErExchangeFanout", "#");
-
-            var consumer2 = new EventingBasicConsumer(channel2);
-            consumer2.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(" [x] Received ErExchangeFanout {0}", message);
-            };
-            channel2.BasicConsume(queue: "free2",
-                autoAck: true,
-                consumer: consumer2);
-
-            using var channel3 = connection.CreateModel();
-            channel3.QueueDeclare(queue: "free3",
-                durable: false,
-                exclusive: false,
-                autoDelete: true,
-                arguments: null);
-
-            channel3.QueueBind("free3", "SyncDataExchangeTopic", "#");
-
-            var consumer3 = new EventingBasicConsumer(channel3);
-            consumer3.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(" [x] Received SyncDataExchangeTopic {0}", message);
-            };
-            channel3.BasicConsume(queue: "free3",
-                autoAck: true,
-                consumer: consumer3);
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+            return channel;
         }
     }
 }
