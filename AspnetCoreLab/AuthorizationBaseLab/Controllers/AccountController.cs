44d3be177bf45713a7b170e796b5bc9e424e29c2
fengrui <fengrui358@163.com>
2021-12-27 11:41:09 +0800

    Add AuthorizationBaseLab Project

---
+++ AspnetCoreLab/AuthorizationBaseLab/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationBaseLab.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet(nameof(Login))]
+        public void Login()
+        {
+
+        }
+    }
+}
---
+++ AspnetCoreLab/AuthorizationBaseLab/README.md
@@ -0,0 +1,13 @@
+# AuthorizationBaseLab
+
+## 介绍
+
+该项目用于测试 asp.net core 的基础认证功能。
+
+## 认证
+
+### cookie 认证
+
+
+
+### jwt 认证
