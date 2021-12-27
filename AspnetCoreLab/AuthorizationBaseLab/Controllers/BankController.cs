f35d2c54379c352ab233d3e2aa3e3b3ea7c73c26
fengrui <fengrui358@163.com>
2021-12-27 13:48:28 +0800

    增加测试接口

---
+++ AspnetCoreLab/AuthorizationBaseLab/Controllers/BankController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationBaseLab.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class BankController : ControllerBase
+    {
+        /// <summary>
+        /// 查询银行信息
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet(nameof(QueryBankInfo))]
+        public Task<string> QueryBankInfo()
+        {
+            return Task.FromResult("工商银行");
+        }
+    }
+}
