5178a182b8608922f21683e87d2181e053e6085a
fengrui <fengrui358@163.com>
2021-12-28 17:43:57 +0800

    test xss

---
+++ AspnetCoreLab/AuthorizationBaseLab/Pages/Shared/Xss.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthorizationBaseLab.Pages.Shared
+{
+    public class XssModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
